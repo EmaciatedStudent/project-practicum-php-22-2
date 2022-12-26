5ecd2c1fb4017669e1cac28e17ce8df0cc1506a4
EmaciatedStudent <yana.laperdina@gmail.com>
2022-12-26 16:14:41 +0500

    Task 6

--- http.php
+++ http.php
@@ -9,6 +9,7 @@ use Tgu\Laperdina\Exceptions\HttpException;
 
 require_once __DIR__ . '/vendor/autoload.php';
 
+$conteiner = require __DIR__ .'/bootstrap.php';
 $request = new Request($_GET, $_SERVER, file_get_contents('php://input'));
 
 try {
@@ -17,35 +18,31 @@ try {
 
 catch (HttpException $exception) {
     (new ErrorResponse($exception->getMessage()))->send();
-
     return;
 }
 
 try {
     $method = $request->method();
 }
+
 catch (HttpException $exception) {
     (new ErrorResponse($exception->getMessage()))->send();
-
     return;
 }
 
 $routes =[
-    'POST'=>['/posts/comment'=>new CreateComment (
-        new SqliteCommentsRepository (
-            new PDO('sqlite:'.__DIR__.'/blog.sqlite')))],
-    'DELETE'=>['/post/delete' => new DeletePost (
-        new SqlitePostRepository (
-            new PDO('sqlite:'.__DIR__.'/blog.sqlite')))],
+    'GET'=>['/users/show' => FindByUsername::class],
+    'POST'=>['/users/create' => CreateUser::class]
 ];
+
 
 if (!array_key_exists($path,$routes[$method])) {
     (new ErrorResponse('Not found'))->send();
-
     return;
 }
 
-$action = $routes[$method][$path];
+$actionClassName  = $routes[$method][$path];
+$action = $conteiner->get($actionClassName);
 
 try {
     $response = $action->handle($request);
@@ -53,6 +50,5 @@ try {
 }
 catch (Exception $exception) {
     (new ErrorResponse($exception->getMessage()))->send();
-
     return;
 }
--- index.php
+++ index.php
@@ -2,11 +2,22 @@
 
 require_once __DIR__ . '/vendor/autload.php';
 
-spl_autoload_register(function ($class) {
-    $file = str_replace('\\', DIRECTORY_SEPARATOR, $class);
-    $file = str_replace('Class_', 'Class' . DIRECTORY_SEPARATOR, $file) . '.php';
+//spl_autoload_register(function ($class) {
+//    $file = str_replace('\\', DIRECTORY_SEPARATOR, $class);
+//    $file = str_replace('Class_', 'Class' . DIRECTORY_SEPARATOR, $file) . '.php';
+//
+//    if (file_exists($file)) {
+//        require $file;
+//    }
+//});
 
-    if (file_exists($file)) {
-        require $file;
-    }
-});
+function someFunction(bool $one, int $two = 42): string {
+    return $one . $two;
+}
+
+$reflection = new ReflectionFunction('someFunction');
+echo $reflection->getReturnType()->getName()."\n";
+
+foreach ($reflection->getParameters() as $parameter) {
+    echo $parameter->getName().'['.$parameter->getType()->getName()."]\n";
+}
